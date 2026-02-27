7815d12e01bc4a10a144ee60967deeb1c2e2bdd0
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air-7.local>
2026-02-27 14:36:19 +0530

    Final Vercel static fix: simpler config + whitenoise storage adjustment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -139,9 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio' / 'static',
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # AWS SETTINGS
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
