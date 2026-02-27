cfcdb13d08ede6305fba2245b75e48b3da12657f
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air-7.local>
2026-02-27 15:23:37 +0530

    Re-enable AWS S3 for media storage

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -156,13 +156,13 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
 
-# if AWS_STORAGE_BUCKET_NAME:
-#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
-#     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
-#     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# else:
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if AWS_STORAGE_BUCKET_NAME:
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 # EMAIL CONFIGURATION (Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
