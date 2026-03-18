163dfe72bc945bf17fc2605406eeb2df61ac27c3
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air-7.local>
2026-03-18 21:48:43 +0530

    Fix Vercel build externally-managed environment with venv wrapper

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Building Vercel Project..."
-python3 -m pip install -r requirements.txt --break-system-packages
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
