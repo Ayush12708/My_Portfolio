42916067e3e30bfb2bdf4814ee2ce7e30b1de804
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air-7.local>
2026-03-18 21:46:04 +0530

    Fix Vercel outwardly-managed Python pip block

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building Vercel Project..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
